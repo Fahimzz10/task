c406a283a67ade2e9605ed6845c2d29c041b95c9
unknown <fahim.sel10@gmail.com>
2023-02-09 19:21:30 +0600

    Section12

--- NewPro/src/main/java/org/example/Section12.java
+++ NewPro/src/main/java/org/example/Section12.java
@@ -3,7 +3,10 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Iterator;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,5 +34,23 @@ public class Section12 {
         driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
 
         System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
+    }
+    public static void section_assignment2(){
+        driver.get("http://qaclickacademy.com/practice.php");
+
+
+
+        WebElement table=driver.findElement(By.id("product"));
+        System.out.println(table.findElements(By.tagName("tr")).size());
+        System.out.println(table.findElements(By.tagName("tr")).get(0).findElements(By.tagName("th")).size());
+        List<WebElement> secondrow=table.findElements(By.tagName("tr")).get(2).findElements(By.tagName("td"));
+
+        System.out.println(secondrow.get(0).getText());
+
+        System.out.println(secondrow.get(1).getText());
+        System.out.println(secondrow.get(2).getText());
+
+    }
+
     }
 }
