71c582698c05479dde1a3371ac0a2283a00eb01b
Fahim <124136236+Fahimzz10@users.noreply.github.com>
2023-02-12 17:59:36 +0600

    Update Section12.java

--- NewPro/src/main/java/org/example/Section12.java
+++ NewPro/src/main/java/org/example/Section12.java
@@ -82,6 +82,27 @@ public class Section12 {
         int responsecode = conn.getResponseCode();
         System.out.println(responsecode);
     }
+    
+    public static void soft_assert() throws IOException {
+        System.setProperty("web-driver.chrome.driver", "D:\\Automation\\Browsers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        List<WebElement> links=   driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        SoftAssert a =new SoftAssert();
+        for(WebElement link : links) {
+            String url= link.getAttribute("href");
+            HttpURLConnection   conn= (HttpURLConnection)new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            int respCode = conn.getResponseCode();
+            System.out.println(respCode);
+            a.assertTrue(respCode<400, "The link with Text"+link.getText()+" is broken with code" +respCode);
+
+        }
+        a.assertAll();
+    }
 
 
 
